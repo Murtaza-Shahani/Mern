4ce3ca4266905885e0462d51f6b5a619557931d5
Murtaza-Shahani <murtazashahani2021@gmail.com>
2024-05-22 02:07:00 +0500

    second

--- Server/index.js
+++ Server/index.js
@@ -1,34 +1,57 @@
-const express = require("express")
+const express = require("express");
 const app = express();
-const mongoose = require("mongoose")
-const cors = require("cors")
+const mongoose = require("mongoose");
+const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
 const UserModel = require('./models/Users');
 
+mongoose.connect("mongodb+srv://murtazashahani2023:MERN1@mern1.qgjxcqd.mongodb.net/mern1?retryWrites=true&w=majority&appName=mern1");
 
-mongoose.connect("mongodb+srv://murtazashahani2023:MERN1@mern1.qgjxcqd.mongodb.net/mern1?retryWrites=true&w=majority&appName=mern1")
-app.get('/getUser',(req,res)=>{
-    UserModel.find({}).then(function(users){
-        res.json(users)
-    }).catch(function(error){
-        res.json(error)
-    })
-})
+// Get all users
+app.get('/getUser', (req, res) => {
+    UserModel.find({})
+        .then(function(users){
+            res.json(users);
+        })
+        .catch(function(error){
+            res.json(error);
+        });
+});
 
-app.post('/createUser',async(req, res)=>{
-    const user=req.body;
+// Create a new user
+app.post('/createUser', async (req, res) => {
+    const user = req.body;
     const newUser = new UserModel(user);
     await newUser.save();
-    res.json(user);
+    res.json(newUser);
+});
 
+// Update a user by name
+app.put('/updateUser/:name', async (req, res) => {
+    const name = req.params.name;
+    const updates = req.body;
+    try {
+        const updatedUser = await UserModel.findOneAndUpdate({ name }, updates, { new: true });
+        res.json(updatedUser);
+    } catch (error) {
+        res.status(500).json({ message: "Failed to update user." });
+    }
+});
 
-})
-
-
+// Delete a user by name
+app.delete('/deleteUser/:name', async (req, res) => {
+    const name = req.params.name;
+    try {
+        await UserModel.findOneAndDelete({ name });
+        res.json({ message: "User deleted successfully." });
+    } catch (error) {
+        res.status(500).json({ message: "Failed to delete user." });
+    }
+});
 
 const port = 3000;
-app.listen(port, ()=>{
-    console.log(`app is listening at ${port}`)
-})
+app.listen(port, () => {
+    console.log(`app is listening at ${port}`);
+});
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,57 +1,88 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import axios from 'axios'  // this used for req response from server to client and viceversa
-
+import { useEffect, useState } from 'react';
+import './App.css';
+import axios from 'axios';
 
 function App() {
-const [users,setUsers]= useState([])
+    const [users, setUsers] = useState([]);
+    const [name, setName] = useState('');
+    const [age, setAge] = useState('');
+    const [updateName, setUpdateName] = useState('');
 
-const [name, setName] = useState()
-const [age, setAge] =   useState()
+    useEffect(() => {
+        fetchUsers();
+    }, []);
 
-  useEffect(()=>{
-    axios.get("http://localhost:3000/getUser")   //fetching data from db using get user url
-    .then((users)=>{
-      setUsers(users.data)
-    }).catch(e=> console.log(e))
+    const fetchUsers = () => {
+        axios.get("http://localhost:3000/getUser")
+            .then(response => {
+                setUsers(response.data);
+            })
+            .catch(error => console.error(error));
+    };
 
-  }, [])
+    const handleCreateUser = () => {
+        axios.post("http://localhost:3000/createUser", { name, age })
+            .then(response => {
+                setName('');
+                setAge('');
+                fetchUsers();
+            })
+            .catch(error => console.error(error));
+    };
 
+    const handleUpdateUser = () => {
+        axios.put(`http://localhost:3000/updateUser/${updateName}`, { name, age })
+            .then(response => {
+                setName('');
+                setAge('');
+                setUpdateName('');
+                fetchUsers();
+            })
+            .catch(error => console.error(error));
+    };
 
-  const Submit  =()=>{
-    axios.post("http://localhost:3000/createUser", {name, age}) // sending the data to db using CreateUser url
-    .then((users)=>{
-      console.log(users)
-    }).catch(e=> console.log(e))
+    const handleDeleteUser = (name,age) => {
+        axios.delete(`http://localhost:3000/deleteUser/${name,age}`)
+            .then(response => {
+                fetchUsers();
+            })
+            .catch(error => console.error(error));
+    };
 
+    const handleEditUser = (user) => {
+        setName(user.name);
+        setAge(user.age);
+        setUpdateName(user.name);
+    };
 
-  }
-  
-
-  return (
-    <>
-    <div className='center'>
-    <h2>First Mern App</h2>
-    {
-      users.map(user=>{
-        return <div>
-          <h3> {user.name}</h3>
-          <h3>{user.age}</h3>
-        </div>
-      })
-    }
-    <label>NAME:</label>
-    <input type='text' onChange={(e)=>setName(e.target.value)}/>
-    <br/>
-    <label>Email</label>
-    <input type="text" onChange={(e)=>setAge(e.target.value)}/> 
-
-    <br/>
-
-    <button onClick={Submit}>Create User</button>
-    </div>
-         </>
-  )
+    return (
+        <>
+            <div className='center'>
+                <h2>First Mern App</h2>
+                {
+                    users.map(user => (
+                        <div key={user._id}>
+                            <h3> {user.name}</h3>
+                            <h3>{user.age}</h3>
+                            <button onClick={() => handleEditUser(user)}>Edit</button>
+                            <button onClick={() => handleDeleteUser(user.name)}>Delete</button>
+                        </div>
+                    ))
+                }
+                <label>Name:</label>
+                <input type='text' value={name} onChange={(e) => setName(e.target.value)} />
+                <br />
+                <label>Age:</label>
+                <input type='text' value={age} onChange={(e) => setAge(e.target.value)} />
+                <br />
+                {updateName ? (
+                    <button onClick={handleUpdateUser}>Update User</button>
+                ) : (
+                    <button onClick={handleCreateUser}>Create User</button>
+                )}
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
